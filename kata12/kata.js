ff69594d7cc2481d9d9582d61ad0fbeccf4d9b88
FurkanPortakal <furkanportakalx@gmail.com>
2020-12-31 15:29:17 +0300

    kata12

---
+++ kata12/kata.js
@@ -0,0 +1,29 @@
+const digital_root = (n) => {
+  if (n < 10) return n;
+  let out = 0;
+  while (n > 0) {
+    out += Math.floor(n % 10);
+    n = n / 10;
+  }
+  return digital_root(out);
+};
+
+// function digital_root(n) {
+//   const arr = n.toString().split("");
+//   const len = arr.length;
+//   let result = 0;
+//   if (len < 2 && len >= 0) {
+//     return n;
+//   } else {
+//     for (let i = 0; i < len; i++) {
+//       result += parseInt(arr[i]);
+//     }
+//     if (result >= 10) {
+//       return digital_root(result);
+//     } else {
+//       return result;
+//     }
+//   }
+// }
+
+console.log(digital_root(493193));
